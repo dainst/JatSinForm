ca46f487edc28e4b6dc8e74acfae417b2284cd95
marcelriedel <Marcel.Riedel@dainst.de>
2026-06-15 17:42:30 +0200

    outsource app and domain configs into separated file

---
+++ configs/app-config.js
@@ -0,0 +1,6 @@
+// app base url:
+const appBaseUrl = "http://localhost:3000/" 
+// path to xml directory in base-domain
+const xmlFolder = "xml-documents";
+// load editor-xml from localStorage
+const xmlFromEditor = true;
